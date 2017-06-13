2fb24e881f9c4c053e205481a6a6c72388fec3cd
Wayne Chiu <chaoweichiu@gmail.com>
2017-06-13 10:34:05 +0800

    add high level cal container

--- src/components/Calendar/index.js
+++ src/components/Calendar/index.js
@@ -2,19 +2,151 @@ import React, { Component }         from 'react'
 import styled                       from 'styled-components'
 import _                            from 'lodash'
 import moment                       from 'moment'
-import getDatesArr                  from '../../lib/helpers/calendarDate'
 
-
+/* Helpers */
+import getDatesArr from '../../lib/helpers/calendarDate'
 const weekdaysArr = ['日', '一', '二', '三', '四', '五', '六']
+const timeNow = moment()
+const fullDays = ['2017-06-03', '2017-06-15']
+const almostFullDays = ['2017-06-05', '2017-06-07']
+
+class Calendar extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      displayDate  : timeNow.format('YYYY-MM-DD'),
+      activeDate   : '2017-06-06',
+      displayDates : getDatesArr({
+                      date:timeNow.format('YYYY-MM'),
+                      active:'2017-06-06',
+                      fullDays,
+                      almostFullDays
+                    })
+    }
+
+    this.upSwitch     = this.upSwitch.bind(this)
+    this.downSwitch   = this.downSwitch.bind(this)
+    this.onDateClick  = this.onDateClick.bind(this)
+  }
+
+  upSwitch(){
+    let futureMonth = moment(this.state.displayDate).clone().add(1,'months')
+    this.setState({
+      displayDate: futureMonth.format('YYYY-MM-DD'),
+      displayDates: getDatesArr({
+        date:futureMonth.format('YYYY-MM'),
+        active: this.state.activeDate,
+        fullDays,
+        almostFullDays,
+      })
+    })
+  }
+
+  onDateClick(date){
+    this.setState({
+      activeDate:date,
+      displayDates:this.state.displayDates.map(obj=>{
+        return obj.date == date
+          ? Object.assign({}, {...obj}, {active: true})
+          : Object.assign({}, {...obj}, {active: false})
+      })
+    })
+  }
+
+  downSwitch(){
+    let futureMonth = moment(this.state.displayDate).clone().subtract(1,'months')
+    this.setState({
+      displayDate: futureMonth.format('YYYY-MM-DD'),
+      displayDates: getDatesArr({
+        date:futureMonth.format('YYYY-MM'),
+        active: this.state.activeDate,
+        fullDays,
+        almostFullDays,
+      })
+    })
+  }
+
+  render(){
+    const {
+      displayDate,
+    } = this.state
+
+    const {
+      upSwitch,
+      downSwitch,
+      onDateClick,
+    } = this
+
+    return (
+      <CalendarContainer {...this.props}>
+        <Top>
+          <SwitchBtn onClick={()=>upSwitch()}>{`<`}</SwitchBtn>
+            <div>
+              <div>year:{moment(displayDate).format('YYYY')}</div>
+              <div>month:{moment(displayDate).format('MM')}</div>
+            </div>
+          <SwitchBtn onClick={()=>downSwitch()}>{`>`}</SwitchBtn>
+        </Top>
+        <Bottom>
+          {weekdaysArr.map((weekday,index)=>{
+            return (<DayText key={`${index}_weekday`}>{weekday}</DayText>)
+          })}
+        </Bottom>
+        <Bottom>
+          {this.state.displayDates.map((date,index)=>{
+            if(date.active){
+              return (<ActiveDayText key={`${index}_date`}>{date.displayDate}</ActiveDayText>)
+            } else if (date.state == 'full'){
+              return (<FullDayText key={`${index}_date`}>full:{date.displayDate}</FullDayText>)
+            } else if (date.state == 'almostFull'){
+              return (<AlmostFullDayText onClick={()=>onDateClick(date.date)} key={`${index}_date`}>almost:{date.displayDate}</AlmostFullDayText>)
+            } else {
+              return (<NopeDayText key={`${index}_date`}>{date.displayDate}</NopeDayText>)
+            }
+          })}
+        </Bottom>
+      </CalendarContainer>
+    )
+  }
+}
+
+export default Calendar
+//width: ${(props)=>`${props.maxWidth / 10}vmin`};
+/* Style Component */
 const CalendarContainer = styled.div`
-  width:100%;
-  background-color:gray;
+  background-color: #FFFFFF;
+  border-radius:2%;
+  width:${props=>`${props.viewWidth*100}vw`};
+  display:flex;
+  flex-direction: column;
+
+  @media (max-width: 500px) {
+    width: ${props=>`${props.viewWidthForMobile*100}vw`};
+    margin-left: ${props=>`${(100-props.viewWidthForMobile*100)/2}vw`};
+  }
 `
 const Top = styled.div`
   width:100%;
-  background-color: green;
   display:flex;
+  justify-content: space-between;
+  padding: 3vmin;
 `
+const SwitchBtn = styled.div`
+  background-color:#DCDCDC;
+  width: ${(props)=>`${props.maxWidth / 10/10}vmin`};
+  height: ${(props)=>`${props.maxWidth / 10/10 -1.5}vmin`};
+  border-radius: 100%;
+  font-size: ${(props)=>`${props.maxWidth / 10/10/2}vmin`};
+  padding-top: 1.5vmin;
+  text-align:center;
+
+  &:hover{
+    background-color: black;
+    cursor: pointer;
+    color:white;
+  }
+`
+
 const Bottom = styled.div`
   width: 100%;
   background-color:pink;
@@ -23,7 +155,6 @@ const Bottom = styled.div`
   flex-direction:row;
   flex-wrap: wrap;
 `
-
 const DayText = styled.div`
   width: ${props=>`${(100/7)}%`};
   text-align:center;
@@ -60,100 +191,3 @@ const NopeDayText = styled(DayText)`
     cursor: not-allowed;
   }
 `
-
-const timeNow = moment()
-const fullDays = ['2017-06-03', '2017-06-15']
-const almostFullDays = ['2017-06-05', '2017-06-07']
-
-class Calendar extends Component {
-  constructor(props){
-    super(props)
-    this.state = {
-      displayDate      : timeNow.format('YYYY-MM-DD'),
-      activeDate       : '2017-06-06',
-      displayDates     : getDatesArr({
-                          date:timeNow.format('YYYY-MM'),
-                          active:'2017-06-06',
-                          fullDays,
-                          almostFullDays
-                        })
-    }
-
-    this.upSwitch     = this.upSwitch.bind(this)
-    this.downSwitch   = this.downSwitch.bind(this)
-    this.onDateClick  = this.onDateClick.bind(this)
-
-  }
-  upSwitch(){
-    let futureMonth = moment(this.state.displayDate).clone().add(1,'months')
-    this.setState({
-      displayDate: futureMonth.format('YYYY-MM-DD'),
-      displayDates: getDatesArr({
-        date:futureMonth.format('YYYY-MM'),
-        active: this.state.activeDate,
-        fullDays,
-        almostFullDays,
-      })
-    })
-  }
-  onDateClick(date){
-    this.setState({
-      activeDate:date,
-      displayDates:this.state.displayDates.map(obj=>{
-        return obj.date == date
-          ? Object.assign({}, {...obj}, {active: true})
-          : Object.assign({}, {...obj}, {active: false})
-      })
-    })
-  }
-  downSwitch(){
-    let futureMonth = moment(this.state.displayDate).clone().subtract(1,'months')
-    this.setState({
-      displayDate: futureMonth.format('YYYY-MM-DD'),
-      displayDates: getDatesArr({
-        date:futureMonth.format('YYYY-MM'),
-        active: this.state.activeDate,
-        fullDays,
-        almostFullDays,
-      })
-    })
-  }
-  render(){
-    const { displayDate } = this.state
-    return (
-      <CalendarContainer>
-        <Top>
-          <button onClick={()=>this.upSwitch()}>up</button>
-          <div>year:{moment(displayDate).format('YYYY')}</div>
-          <div>month:{moment(displayDate).format('MM')}</div>
-          <button onClick={()=>this.downSwitch()}>left</button>
-        </Top>
-        <Bottom>
-          {weekdaysArr.map((weekday,index)=>{
-            return (<DayText key={`${index}_weekday`}>{weekday}</DayText>)
-          })}
-        </Bottom>
-        {console.time('hi')}
-        <Bottom>
-          {this.state.displayDates.map((date,index)=>{
-            if(date.active){
-              return (<ActiveDayText key={`${index}_date`}>{date.displayDate}</ActiveDayText>)
-            } else if (date.state == 'full'){
-              return (<FullDayText key={`${index}_date`}>full:{date.displayDate}</FullDayText>)
-            } else if (date.state == 'almostFull'){
-              return (<AlmostFullDayText onClick={()=>this.onDateClick(date.date)} key={`${index}_date`}>almost:{date.displayDate}</AlmostFullDayText>)
-            } else {
-              return (<NopeDayText key={`${index}_date`}>{date.displayDate}</NopeDayText>)
-            }
-          })}
-        </Bottom>
-        {console.timeEnd('hi')}
-
-
-
-      </CalendarContainer>
-    )
-  }
-}
-
-export default Calendar
--- src/pages/CalendarPage/index.js
+++ src/pages/CalendarPage/index.js
@@ -1,15 +1,32 @@
 import React from 'react'
 import Calendar from '../../components/Calendar'
+import styled from 'styled-components'
 
-const activeDays = [
-  {date: '2017-05-01' , status: 'active'},
-  {date: '2017-05-07' , status: 'full'},
-  {date: '2017-05-17' , status: 'almost'}
-]
+const MessageContainer = styled.div`
+  width: ${(props)=>`${props.viewWidth*100}vw`};
+  background-color:gray;
+  height: 500px;
 
+  @media (max-width: 500px) {
+    width: ${props=>`${props.viewWidthForMobile*100}vw`};
+    margin-left: ${props=>`${(100-props.viewWidthForMobile*100)/2}vw`};
+  }
+`
+const CalendarPageContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  text-align:center;
 
-const CalendarPage = () => (<div>
-    <Calendar maxWidth={900} activeDays={activeDays}/>
-  </div>)
+  @media (max-width: 500px) {
+    flex-direction: column;
+  }
+`
+
+const CalendarPage = () => (
+  <CalendarPageContainer>
+    <Calendar         viewWidth={70/100} viewWidthForMobile={95/100}/>
+    <MessageContainer viewWidth={30/100} viewWidthForMobile={95/100}/>
+  </CalendarPageContainer>
+)
 
 export default CalendarPage
