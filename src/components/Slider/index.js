a3bb6e5601008d445678cd6b27605f0e7ad228c3
Wayne Chiu <chaoweichiu@gmail.com>
2017-06-13 19:10:47 +0800

    add slider component

---
+++ src/components/Slider/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import styled from 'styled-components'
+
+class Slider extends Component {
+  constructor(props){
+    super(props)
+  }
+  render(){
+    return (<div>
+      Slider
+    </div>)
+  }
+}
+
+export default Slider
--- src/pages/SlidePage/index.js
+++ src/pages/SlidePage/index.js
@@ -1,6 +1,11 @@
 import React from 'react'
 import styled from 'styled-components'
-
-const SlidePage = () => (<div>SlidePage</div>)
+import Slider from '../../components/Slider'
+const img1800x1000Urls = [
+  'https://www.happykeys.com/uploads/new_images/thumbs/1065_2_ca3.jpg',
+  'https://www.azamaraclubcruises.com/sites/default/files/heros/shanghai-china.jpg',
+  'https://madebysidecar.com/assets/products/thumbs/web-grid-img-1.jpg'
+]
+const SlidePage = () => (<Slider imgs={img1800x1000Urls}/>)
 
 export default SlidePage
