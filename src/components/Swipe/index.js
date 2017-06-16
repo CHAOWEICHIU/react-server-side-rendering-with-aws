22ec1d763a96adfe2ccbd923ae6e7d26bd777d90
Wayne Chiu <chaoweichiu@gmail.com>
2017-06-16 09:41:13 +0800

    add react-hammerjs

--- src/components/Swipe/index.js
+++ src/components/Swipe/index.js
@@ -1,7 +1,47 @@
-import React from 'react'
+import React, { Component } from 'react'
+import Hammer from 'react-hammerjs'
+import styled from 'styled-components'
+class Swipe extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      handleTap: false,
+      handleSwipe: false,
+    }
+    this.handleTap = this.handleTap.bind(this)
+    this.handleSwipe = this.handleSwipe.bind(this)
+  }
+  handleTap(){
+    console.log('handleTap');
+    this.setState({
+      handleTap: true,
+      handleSwipe: false,
+    })
+  }
+  handleSwipe(){
+    console.log('handleSwipe');
+    this.setState({
+      handleTap: false,
+      handleSwipe: true,
+    })
+  }
+  render(){
+    const { handleTap, handleSwipe } = this
+    console.log(this.state);
+    return (<div>
+      <div>Tap: {this.state.handleTap.toString()}</div>
+      <div>swipe: {this.state.handleSwipe.toString()}</div>
+      <div></div>
+      <Hammer onTap={handleTap} onSwipe={handleSwipe}><AreaForTouch>Tap Me</AreaForTouch></Hammer>
+    </div>)
 
-const Swipe = () => (<div>
-  Swipe
-</div>)
+  }
+}
 
 export default Swipe
+
+const AreaForTouch = styled.div`
+  width: 100%;
+  height: 50vh;
+  background-color: green;
+`
